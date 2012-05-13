e4ebb47daa6658d2edcb8cb6ef8bd7b277f60f2d
Willie Wheeler <willie.wheeler@gmail.com>
2012-05-13 14:07:51 -0700

    Added unit test

---
+++ src/test/java/com/springinpractice/ch09/comment/service/impl/RichTextFilterTests.java
@@ -0,0 +1,43 @@
+package com.springinpractice.ch09.comment.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+/**
+ * @author Willie Wheeler (willie.wheeler@gmail.com)
+ */
+@ContextConfiguration(locations = "/spring/beans-service-richtext.xml")
+public class RichTextFilterTests extends AbstractJUnit4SpringContextTests {
+	
+	// Class under test
+	private RichTextFilter filter;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.filter = applicationContext.getBean(RichTextFilter.class);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		this.filter = null;
+	}
+	
+	@Test
+	public void testAWithJavaScriptUrls() {
+		String in = "<a href=\"javascript:alert('hi')\">Hi</a>";
+		String out = "<p><a rel=\"nofollow\">Hi</a></p>";
+		assertEquals(out, filter.filter(in));
+	}
+	
+	@Test
+	public void testList() {
+		String in = "* Item 1\n* Item 2\n";
+		String out = "<ul> \n<li>Item 1</li> \n<li>Item 2</li> </ul>";
+		assertEquals(out, filter.filter(in));
+	}
+}
